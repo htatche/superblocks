ffe0352011a7f54c457faa4debb528449dc86d79
Herve Tatche <herve.tatche@gmail.com>
2015-03-27 00:52:54 -0400

    Code refactor
    Also added a red flag to avoid the setInterval() move and the user
    input move to call update_grid() at the same time

--- new_tetris.js
+++ new_tetris.js
@@ -21,6 +21,7 @@ var shapes = [
 
 var nshape = 0;
 var falling_shape = null;
+var flag = false;
 
 function initialize_grid() {
 	for (var x=0; x < ROWS; ++x) {
@@ -80,9 +81,10 @@ function loop_move(direction, dfd) {
 	if (y < HEIGHT && !collision) {	
 
 		setTimeout(function() {
-			tween = move.call(parent, direction, dfd);
+			flag = true;	
+			tween = move.call(parent, direction);
 			tween.onComplete.add(loop_move.bind(parent, direction, dfd), parent);	
-		}, 100);
+		}, 500);
 
 	} else {
 		dfd.resolve();
@@ -90,19 +92,11 @@ function loop_move(direction, dfd) {
 
 }
 
-function move(direction, dfd) {
+function move(direction) {
 	var tween = game.add.tween(this);
 	var next_position = getNextPosition.call(this, direction);
 	var previous_x = this.x;
 	var previous_y = this.y;
-
-	// if (tweens_chain.length == 0 ) {
-	// 	move_tween.call(tween, this, next_position);			
-	// } else {		
-	// 	tweens_chain[tweens_chain.length-1].onComplete.add(
-	//   	move_tween.bind(tween, this, next_position)
-	//   , this);		
-	// }
 
   tween.to(
     next_position,
@@ -122,36 +116,29 @@ function popTweensChain () {
 	tweens_chain.pop(this);
 }
 
-function move_tween(shape, position) {
-	var previous_x = shape.x;
-	var previous_y = shape.y;
-
-
-	
-}
-
 function detect_collision(direction) {
 	var collision = false;
 	var next_position = getNextPosition.call(this, direction);	
 
-	for (var i=0; i < COLUMNS; ++i) {
-		for (var j=0; j < ROWS; ++j) {
+	for (var l=0; l < this.children.length; ++l) {
+		var block = this.children[l];
+		var next_axis = posToAxis(next_position.x + block.x, next_position.y + block.y);				
 
-			for (var l=0; l < this.children.length; ++l) {
-				var block = this.children[l];
-				var next_axis = posToAxis(next_position.x + block.x, next_position.y + block.y);				
+		// Blocks collision
 
-				if (i == next_axis[0] && j == next_axis[1]) {
-					if (grid[j][i] != null && grid[j][i] != nshape) {
-						return collision = true;
-					}
-				}		
-
-				// Add walls collision		
-
+		try {
+			if (grid[next_axis[1]][next_axis[0]] != null && grid[next_axis[1]][next_axis[0]] != nshape) {
+				return collision = true;
 			}
-		}		
-	}	
+		} catch (e) {
+			// Out of grid range
+		}
+
+		// Walls collision		
+
+		
+
+	}
 
 	return collision;
 }
@@ -213,37 +200,7 @@ function update_grid(previous_x, previous_y, block_color) {
 
 	}
 
-
-
-	// for (var i=0; i < COLUMNS; ++i) {
-	// 	for (var j=0; j < ROWS; ++j) {
-
-	// 		for (var l=0; l < this.children.length; ++l) {
-	// 			var block = this.children[l];
-
-	// 			previous_axis = posToAxis(previous_x + block.x, previous_y + block.y);
-	// 			axis 					= posToAxis(this.x + block.x, this.y + block.y);				
-
-	// 			if (i == previous_axis[0] && j == previous_axis[1]) {
-	// 		    grid[j][i] = null; //{busy: false, color: null};
-	// 			}	
-
-	// 		}
-
-	// 		for (var l=0; l < this.children.length; ++l) {
-	// 			var block = this.children[l];
-
-	// 			previous_axis = posToAxis(previous_x + block.x, previous_y + block.y);
-	// 			axis 					= posToAxis(this.x + block.x, this.y + block.y);				
-
-	// 			if (i == axis[0] && j == axis[1]) {
-	// 		    grid[j][i] = nshape; //{busy: true, color: block_color};
-	// 			}				
-
-	// 		}
-
-	// 	}		
-	// }
+	flag = false;
 
 }
 
@@ -307,7 +264,7 @@ function update() {
 		direction = "right";
 	}	
 
-	move.call(falling_shape, direction, loop=false);
+	if (!flag) move.call(falling_shape, direction);
 
 	if (direction) {
 		// var collision = detect_collision.call(falling_shape, direction);
